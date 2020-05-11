c54febe803aff57569ee345979a52fa9c973e55e
Parker <parker.hunter@guidepointsecurity.com>
2020-05-11 12:19:50 -0400

    Added Basic setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='cloud_enum',
+	version='1.0',
+	description='Cloud Enum Project',
+	long_description=long_description,
+	url='https://github.com/StormCTF/cloud_enum',
+	packages=find_packages(where='src'),
+	packages=setuptools.find_packages(),
+	python_requires='>=3.4'
+)
